f40ee20f3e256dbce4e0c3138d8a256dbd4b2999
NKS <iscream433@gmail.com>
2024-03-15 17:31:41 +0900

    ひとまずの完成形

--- src/App.js
+++ src/App.js
@@ -1,77 +1,220 @@
-import { useEffect, useState } from 'react';
+
+import { useEffect, useState } from "react";
 import Card from "./components/Card";
 import './App.css';
 
 function App() {
-  
-  // let とconst　と　varは再宣言とかが違うだけ
-  // letは同じスコープ内で再宣言できない（varはOK）
-  // 変数imagesに6枚の画像を配列として格納する
-  // 連想配列（＝オブジェクト）を使っている
-  // キーは「num」、値が「img」
-  const images =[
+  let images = [
     {
       num: 1,
-      img: "./img/chopper.png"
+      img: "./img/luffy.png",
+      isMatched: false
     },
     {
       num: 2,
-      img: "./img/luffy.png"
+      img: "./img/zoro.png",
+      isMatched: false
     },
     {
       num: 3,
-      img: "./img/nami.png"
+      img: "./img/nami.png",
+      isMatched: false
     },
     {
       num: 4,
-      img: "./img/sanji.png"
+      img: "./img/sanji.png",
+      isMatched: false
     },
     {
       num: 5,
-      img: "./img/usopp.png"
+      img: "./img/usopp.png",
+      isMatched: false
     },
     {
       num: 6,
-      img: "./img/zoro.png"
+      img: "./img/chopper.png",
+      isMatched: false
     },
   ];
 
-  // useStateを使ってcardの状態を管理 初期値は[]空配列
   const [cards, setCards] = useState([]);
-  // シャッフル
+  const [selectedCards, setselectedCards] = useState([]);
+  const [tries, setTries] = useState(0);
+
   const shuffleImages = () => {
-    // スプレッド関数を用いて6枚の画像のセットをもう1セット作り、合計2セットを配列につっこむ
-    let shuffleImages = [...images, ...images];//2枚ずつ
-      // shuffleImagesにIDプロパティを付与する！！！！！！！（つまりポイント）
-      // shuffleImages.map(  (item,index) => ({item,id:index+1}) );
-      shuffleImages = shuffleImages.map((item, index)=>({...item,id:index+1}));
+    let shuffledImages = [...images, ...images]
+      .map((item, index) => ({...item, id: index + 1}))
+      .sort((a, b) => 0.5 - Math.random());
 
-      // 並び替えを行う
-      shuffleImages.sort((a,b) => 0.5 -Math.random());
-
-
-    // 更新関数を使う
-    setCards(shuffleImages);
+    setCards(shuffledImages);
   }
 
-  useEffect(() =>{
+  useEffect(() => {
     shuffleImages();
-  },[]);
+  }, []);
 
-  console.log(cards);
+  useEffect(() => {
+    if (selectedCards.length === 2){
+      setTimeout(()=>{
+        setselectedCards([]);
+      }, 1000);
+      checkMatch();
+    }
+  }, [selectedCards]);
+
+  useEffect(() => {
+    if (cards.length === 0) return
+
+    const allMatched = cards.every(card => card.isMatched);
+    if (allMatched) {
+      setTimeout(()=>{
+        alert('ゲームクリア！！');
+      }, 500);
+    }
+  }, [cards]);
+
+  const checkMatch = () => {
+    if (selectedCards[0].num === selectedCards[1].num) {
+      let updatedCards = cards.map((card) => {
+        if (card.num === selectedCards[0].num){
+          console.log(cards);
+          return { ...card, isMatched: true}
+        }
+
+        return card
+      });
+      setCards(updatedCards);
+    } else {
+      setTries(prev => prev + 1);
+
+      if (tries > 20) {
+        setTimeout(() => {
+          alert('ゲームオーバー');
+        }, 500);
+      }
+    }
+  }
 
   return (
     <div className="container">
-      <div className="cards-container"> 
-      {cards.map((card)=>(
-        <Card
-          key={card.id}
-          card={card}
-        />
-      ))}
+      <div className="cards-container">
+        {cards.map((card)=>(
+          <Card
+            key={card.id}
+            card={card}
+            selectedCards={selectedCards}
+            setselectedCards={setselectedCards}
+          />
+        ))}
       </div>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+// import { useEffect, useState } from 'react';
+// import Card from "./components/Card";
+// import './App.css';
+
+// function App() {
+  
+//   // let とconst　と　varは再宣言とかが違うだけ
+//   // letは同じスコープ内で再宣言できない（varはOK）
+//   // 変数imagesに6枚の画像を配列として格納する
+//   // 連想配列（＝オブジェクト）を使っている
+//   // キーは「num」、値が「img」
+
+
+//   const images =[
+//     {
+//       num: 1,
+//       img: "./img/chopper.png"
+//     },
+//     {
+//       num: 2,
+//       img: "./img/luffy.png"
+//     },
+//     {
+//       num: 3,
+//       img: "./img/nami.png"
+//     },
+//     {
+//       num: 4,
+//       img: "./img/sanji.png"
+//     },
+//     {
+//       num: 5,
+//       img: "./img/usopp.png"
+//     },
+//     {
+//       num: 6,
+//       img: "./img/zoro.png"
+//     },
+//   ];
+
+//   // useStateを使ってcardの状態を管理 初期値は[]空配列
+//   const [cards, setCards] = useState([]);
+//   const[selectedCards, setselectedCards] =useState([]);
+
+//   // シャッフル
+//   const shuffleImages = () => {
+//     // スプレッド関数を用いて6枚の画像のセットをもう1セット作り、合計2セットを配列につっこむ
+//     let shuffleImages = [...images, ...images];//2枚ずつ
+//       // shuffleImagesにIDプロパティを付与する！！！！！！！（つまりポイント）
+//       // shuffleImages.map(  (item,index) => ({item,id:index+1}) );
+//       shuffleImages = shuffleImages.map((item, index)=>({...item,id:index+1}));
+
+//       // 並び替えを行う
+//       shuffleImages.sort((a,b) => 0.5 -Math.random());
+
+
+//     // 更新関数を使う
+//     setCards(shuffleImages);
+//   }
+
+//   useEffect(() =>{
+//     shuffleImages();
+//   },[]);
+
+//   useEffect(()=>{
+//     if(selectedCards.length === 2){
+//       checkMatch();
+//     }
+//     // console.log(selectedCards);
+//   },[selectedCards]);
+
+//   const checkMatch = () =>{
+//     if(selectedCards[0].num === selectedCards[1].num){
+//       console.log("マッチ")
+//     }else{
+//       console.log("ミスマッチ")
+//     }
+//   }
+
+
+
+
+//   // console.log(cards);
+
+//   return (
+//     <div className="container">
+//       <div className="cards-container"> 
+//       {cards.map((card)=>(
+//         <Card
+//           key={card.id}
+//           card={card}
+//           selectedCards={selectedCards}
+//           setselectedCards={setselectedCards}
+//         />
+//       ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/components/card.js
+++ src/components/card.js
@@ -1,15 +1,50 @@
-const Card = () =>{
-    return(
-        <div className="card">
-            <div className="front">
-                {/* imgタグを使って画像を表示 */}
-                <img src={Card.img} alt="" />
-            </div>
-            <div className="back"></div>
-        </div>
-    );
+import { useEffect, useState } from "react";
+
+const Card = ({ card, selectedCards, setselectedCards}) => {
+  const [isFripped, setIsFripped] = useState(false);
+
+  const handleClick = () => {
+    setselectedCards([...selectedCards, card]);
+  }
+
+  useEffect((()=> {
+    if(selectedCards[0] === card || selectedCards[1] === card || card.isMatched){
+      setIsFripped(true);
+    } else {
+      setIsFripped(false);
+    }
+  }), [selectedCards])
+  return (
+    <div className={isFripped ? "card open" : "card"} onClick={handleClick}>
+      <div className="front">
+        <img src={card.img} alt="" />
+      </div>
+      <div className="back"></div>
+    </div>
+  );
 }
 
-
-// 他のファイルでインポートができるようにする
 export default Card;
+
+
+
+// const Card = ({card, selectedCards,setselectedCards}) =>{
+
+//     const handleClick = () =>{
+//         setselectedCards([...selectedCards, card]);
+//     // console.log("クリックしました");
+//     }
+//     return(
+//         <div className="card" onClick={handleClick}>
+//             <div className="front">
+//                 {/* imgタグを使って画像を表示 */}
+//                 <img src={Card.img} alt="" />
+//             </div>
+//             <div className="back"></div>
+//         </div>
+//     );
+// }
+
+
+// // 他のファイルでインポートができるようにする
+// export default Card;
